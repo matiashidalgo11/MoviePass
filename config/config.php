495789a0083992a92305922b8d63cfb6fc12c341
Agustin San Martin <agus.sanmartin95@gmail.com>
2020-10-11 16:48:09 -0300

    Template, Cuenta added, "HOST_URL" added.

--- config/config.php
+++ config/config.php
@@ -6,3 +6,4 @@ define('VIEWS_PATH','views/');
 
 define('KEY_TMDB', '73738116e9432d71990567aeec929b73');
 define('IMG_BASE_TMBD','https://image.tmdb.org/t/p/');
+define('HOST_URL', '//'.$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF'])."/");

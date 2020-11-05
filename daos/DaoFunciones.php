e0219f6bf52163d136c7bfa1ca28578177345b53
Federico <fedu_zero@hotmail.com>
2020-11-05 19:19:54 -0300

    Arreglado TODOS los problemas de Daos en funcion, cine y room.

--- daos/DaoFunciones.php
+++ daos/DaoFunciones.php
@@ -11,11 +11,6 @@ use daos\DaoMovies as movieDao;
 
 class ProjectionDAO {
     private $connection;
-    const TABLE_IDFUNCION = "idFuncion";
-    const TABLE_IDMOVIE = "idMovie";
-    const TABLE_IDROOM = "idRoom";
-    const TABLE_DATE = "date";
-    const TABLE_HOUR = "time";
 
     public function __construct() {
         
@@ -25,15 +20,13 @@ class ProjectionDAO {
     {
         try
         {
-            $sql = "INSERT INTO funciones (idMovie,idRoom,date,time) VALUES (:idMovie,:idRoom,:date,:time);";
-            $room=$funcion->getRoom();
-            $movie=$funcion->getMovie();
-            $parameters["idMovie"] =$movie->getId();
-            $parameters["idRoom"] =$room->getId();
+            $sql = "INSERT into funciones (idMovie,idRoom,dateF,timeF) values (:idMovie,:idRoom,:date,:time);";
+            $parameters["idMovie"] =$funcion->getRoom()->getId();
+            $parameters["idRoom"] =$funcion->getMovie()>getId();
             $parameters["date"]=$funcion->getDate();
             $parameters["time"]=$funcion->getHour();
             $this->connection = Connection::getInstance();
-            $this->connection->executeNonQuery($sql, $parameters);
+            return $this->connection->executeNonQuery($sql, $parameters);
         }
         catch(Exception $ex)
         {
@@ -41,130 +34,61 @@ class ProjectionDAO {
         }
     }
 
- /* Debo ingresar todos los atributos de Movie en el select y la creacion del objeto. Debo crear un atributo genero en el constructor que viene de base DaoGenre.
-    public function getArrayByIdRoom($idRoom){
-        $funcion_list=array();
-        try{
-            $sql="SELECT f.idFuncion,f.idRoom,f.time,f.date,m.idMovie,m.title
-                    from funciones f
-                    inner join movies m on m.idMovie = f.idMovie
-                    where f.idRoom = $idRoom and concat(f.date,' ',f.time) > now()";
-            $this->connection=Connection::getInstance();
-            $resultSet=$this->connection->execute($sql);
-            foreach ($resultSet as $row) {
-                $movie=new Movie($row["title"],$row["idMovie"]); 
-                $movie->setGenres($this->genero->getByIdMovie($row["idMovie"]));
-                $funcion_list[]=new Funcion($row["idFuncion"],$movie,$idRoom,$row["date"],$row["time"]);
-            }
-            return $funcion_list;
-        }
-        catch(Exception $ex){
-            throw $ex;
-        }
-    }*/
-
-
     public function GetById($id) {
-
+        $funcion = null;
         try
         {
             $parameters['id'] = $id;
-            $room=$funcion->getRoom();
-            $movie=$funcion->getMovie();
-
-            $sql = "SELECT * FROM funciones WHERE id=:id";
-
+            $sql = "SELECT * from funciones where id=:id";
             $this->connection = Connection::getInstance();
-
             $resultSet = $this->connection->Execute($sql, $parameters);
-
             if(!empty($resultSet)) {
-                return $this->mapeo($resultSet);
+                $funcion = $this->mapeo($resultSet[0]);
+                $idRoom = $resultSet[0]["idRoom"];
+                $idMovie = $resultSet[0]["idMovie"];
+                $DaoRoom = new roomDao();
+                $room = $DaoRoom->GetById($idRoom);
+                $DaoMovie = new movieDao();
+                $movie = $DaoMovie->GetById($idMovie);
+                $funcion->setRoom($room);
+                $funcion->setMovie($movie);
+
             }
         }
         catch (PDOException $e)
         {
             throw $e;
         }
-    }
-
-    private function mapeo(){
-       
-        $value = is_array($value) ? $value : []; 
-    
-        $resp = array_map( 
-            function ($p) { 
- 
-                $objet =  new Room( 
-                    $p[DaoFuncion::TABLE_ID], 
-                    $p[DaoFuncion::TABLE_IDMOVIE], 
-                    $p[DaoFuncion::TABLE_IDROOM], 
-                    $p[DaoFuncion::TABLE_DATE], 
-                    $p[DaoFuncion::TABLE_HOUR], 
-                ); 
- 
-                return $objet; 
-            }, 
-            $value 
-        ); 
-        return count($resp) > 1 ? $resp : $resp['0']; 
+        return $funcion;
     }
 
     public function remove ($id)
     {
+        $value =0;
         try
         {
-            $sql = "DELETE FROM funciones WHERE id=:id";  
+            $parameters['id'] = $id;
+            $sql = "DELETE from funciones where id=:id";  
              $this->connection=Connection::getInstance();
-             return $this->connection->ExecuteNonQuery($sql);
+             $value = $this->connection->ExecuteNonQuery($sql,$parameters);
         }
-        catch(Exception $ex){
-        throw $ex;
-    }
-    }
-
-    public function getAllMovies(){
-        $moviesList=array();
-        try{
-            $query="SELECT m.* from projections p
-                    inner join movies m on p.id_movie=m.id_movie
-                    where concat(p.proj_date,' ',p.proj_time) > now()
-                    group by m.id_movie";
-            $this->connection=Connection::getInstance();
-            $results=$this->connection->execute($query);
-            foreach ($results as $row) {
-                $movie=new Movie($row["title"],
-                    $row["id_movie"],
-                    $row["synopsis"],
-                    $row["poster_url"],
-                    $row["video_url"],
-                    $row["length"],
-                    [],
-                    $row["release_date"]);
-                $movie->setGenres($this->genrexM->getByMovieId($row["id_movie"]));
-                $moviesList[]=$movie;
-            }
-            return $moviesList;          
-        }catch(Exception $ex){
+            catch(Exception $ex){
             throw $ex;
         }
+        return $value;
     }
 
     public function GetAllByDate($date) {
         $funcion_list =array();
-
         try
         {
             $parameters['date'] = $date;
-            $sql = "SELECT * FROM funciones where date=:date ORDER BY date";
-
+            $sql = "SELECT * from funciones where dateF=:date ORDER BY dateF";
             $this->connection = Connection::getInstance();
-
             $resultSet = $this->connection->Execute($sql, $parameters);
-
             if(!empty($resultSet)) {
                 foreach ($resultSet as $row) {
-                    return $this->leer($row);
+                    $funcion =$this->mapeo($row);
 
                     $idRoom = $row["idRoom"];
                     $idMovie = $row["idMovie"];
@@ -191,10 +115,9 @@ class ProjectionDAO {
 
     public function GetAll(){
         $funcion_list =array();
-
         try
         {
-            $sql = "SELECT * FROM funciones f where datediff(f.date,(curdate()-1)) > 0 ORDER BY date";
+            $sql = "SELECT * from funciones f where datediff(f.dateF,(curdate()-1)) > 0 order by dateF";
 
             $this->connection = Connection::getInstance();
 
@@ -202,20 +125,15 @@ class ProjectionDAO {
 
             if(!empty($resultSet)) {
                 foreach ($resultSet as $row) {
-                    return $this->leer($row);
-
+                    $funcion =$this->mapeo($row);
                     $idRoom = $row["idRoom"];
                     $idMovie = $row["idMovie"];
-
                     $DaoRoom = new roomDao();
                     $room = $DaoRoom->GetById($idRoom);
-
                     $DaoMovie = new movieDao();
                     $movie = $DaoMovie->GetById($idMovie);
-
                     $funcion->setRoom($room);
                     $funcion->setMovie($movie);
-
                     array_push($funcion_list,$funcion);
                 }
             }
@@ -227,7 +145,36 @@ class ProjectionDAO {
         return funcion_list;
     }
 
-    public function leer($row){
+    public function GetAllList(){
+        $funcion_list =array();
+        try
+        {
+            $sql = "SELECT * from funciones order by dateF";
+            $this->connection = Connection::getInstance();
+            $resultSet = $this->connection->Execute($sql, $parameters);
+            if(!empty($resultSet)) {
+                foreach ($resultSet as $row) {
+                    return $this->mapeo($row);
+                    $idRoom = $row["idRoom"];
+                    $idMovie = $row["idMovie"];
+                    $DaoRoom = new roomDao();
+                    $room = $DaoRoom->GetById($idRoom);
+                    $DaoMovie = new movieDao();
+                    $movie = $DaoMovie->GetById($idMovie);
+                    $funcion->setRoom($room);
+                    $funcion->setMovie($movie);
+                    array_push($funcion_list,$funcion);
+                }
+            }
+        }
+        catch (PDOException $e)
+        {
+            throw $e;
+        }
+        return funcion_list;
+    }
+
+    public function mapeo($row){
         $funcion = new Funcion();
 
         $funcion->setId($row["id"]);
@@ -235,6 +182,43 @@ class ProjectionDAO {
         $funcion->setHour($row["hour"]);
 
         return $funcion;
+    }
+
+    public function GetRowsByDate($date) {
+        try
+        {
+            $parameters['date'] = $date;
+            $sql = "SELECT count(*) from funciones where date>=:date";
+            $this->connection = Connection::getInstance();
+            $resultSet = $this->connection->execute($sql, $parameters);
+            return $resultSet[0]["COUNT(*)"];
+        }
+        catch (PDOException $e)
+        {
+            throw $e;
+        }
+    }
+
+    public function getDate()
+	{
+		$dateList = array();
+        try
+        {
+            $sql = "SELECT dateF as 'Fecha',COUNT(dateF) from funciones f where DATEDIFF(f.date,(CURDATE()-1)) > 0 group by dateF";
+            $this->connection = Connection::getInstance();
+            $resultSet = $this->connection->execute($sql);
+            if(!empty($resultSet)) {
+                foreach ($resultSet as $row) {
+					$date = $row['Fecha'];
+                    array_push($dateList, $date);
+                }
+            }
+        }
+        catch (PDOException $e)
+        {
+            throw $e;
+        }
+        return $dateList;
     }
 
 }

379c1abcdbf8899e314c00106954710375e13a73
Matias Hidalgo <matiashidalgo18@gmail.com>
2020-11-05 22:09:31 -0300

    Login completo

--- views/login.php
+++ views/login.php
@@ -9,14 +9,23 @@
 
                 <form class="form-container border rounded-lg" action="<?= FRONT_ROOT ?>CuentasController/verificar" method="POST">
                         <h3 class="text-center font-eight-bol">Iniciar Sesion</h3>
+
                         <div class="form-group">
                             <label for="exampleInputEmail1">Email</label>
-                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email">
-                            <small id="emailHelp" class="form-text text-muted">Ingrese un email valido.</small>
+                            <input type="email" class="form-control <?=(isset($emailValidator))? $emailValidator : "";?>" id="exampleInputEmail1" aria-describedby="emailHelp" name="email" value="<?=(isset($emailIngresado))? $emailIngresado : "";?>" >
+                            <div class="invalid-feedback">
+                             El email es invalido
+                            </div>
+                            
                         </div>
+
+
                         <div class="form-group">
                             <label for="exampleInputPassword1">Password</label>
-                            <input type="password" class="form-control" id="exampleInputPassword1" name="password">
+                            <input type="password" class="form-control <?=(isset($passValidator))? $passValidator : "";?>" id="exampleInputPassword1" name="password">
+                            <div class="invalid-feedback">
+                             Password incorrecto
+                            </div>
                         </div>
                        
                         <button type="submit" class="btn btn-primary">Entrar</button>
@@ -27,4 +36,6 @@
         </div>
 
 </div>
+
+
 

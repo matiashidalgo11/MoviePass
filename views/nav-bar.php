bcbe8d6634fbefdcc5c06a6ff6c90a932cf76605
Matias Hidalgo <matiashidalgo18@gmail.com>
2020-11-07 13:14:49 -0300

    Errores al unir solucionados

--- views/nav-bar.php
+++ views/nav-bar.php
@@ -5,30 +5,79 @@
       <i class="fas fa-video"> MoviePass</i>
     </a>
 
-  <div class="collapse navbar-collapse " id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto ">
-      
-    <li class="nav-item active">
-        <a class="nav-link" href="#">Cines <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="<?php echo FRONT_ROOT ?>MoviesController/listMovies">Cartelera</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="<?php echo FRONT_ROOT ?>cineController/ShowList">Administrar Cines</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="<?php echo FRONT_ROOT ?>funcionController/listFunciones">Administrar Funciones</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Perfil
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse " id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto ">
+
+        <?php if (-isset($_SESSION['cuenta'])) { ?>
+        <?php if ($_SESSION['cuenta']->getPrivilegios() == 0) { ?>
+          <li class="nav-item">
+            <a class="nav-link" href="<?= FRONT_ROOT ?>#">Administrar Cines</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="<?= FRONT_ROOT ?>#">Administrar Usuarios</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="<?= FRONT_ROOT ?>#">Actualizar Cartelera</a>
+          </li>
+
+        <?php } else { ?>
+
+
+
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Cines <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="<?= FRONT_ROOT ?>MoviesController/listMovies">Cartelera</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="<?= FRONT_ROOT ?>MoviesController/listMovies">Funciones</a>
+          </li>
+
+          
+
+        <?php } }?>
+
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Generos
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <?php foreach ($listGenres as $genre) { ?>
+
+                <form action="<?= FRONT_ROOT ?>MoviesController/listMovieByGenre" method="post">
+
+                  <input type="hidden" value="<?= $genre->getId(); ?>" name="idGenre">
+
+                  <button class="dropdown-item" type="submit"> <?= $genre->getName(); ?> </button>
+
+                </form>
+
+              <?php } ?>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="#">Todo</a>
+            </div>
+          </li>
+          
+      </ul>
+
+
+
+
+
+      <?php if (!isset($_SESSION['cuenta'])) { ?>
+
+        <div class="userOff d-flex align-items-end">
+          <a class="nav-link" href="<?= FRONT_ROOT ?>LoginController/init">Iniciar Sesion</a>
+
+          <a class="nav-link" href="<?= FRONT_ROOT ?>CuentasController/registrarse">Crear Cuenta</a>
         </div>
 
 
@@ -64,11 +113,6 @@
       <?php } else { ?>
 
         <?php if ($_SESSION['cuenta']->getPrivilegios() == 0) { ?>
-
-          <div class="userOff d-flex align-items-end">
-            <a class="nav-link" href="<?= FRONT_ROOT ?>LoginController/init">Admin</a>
-
-          </div>
 
           <div class="userOff d-flex align-items-end">
             <a class="nav-link" href="<?= FRONT_ROOT ?>LoginController/init">Admin</a>
